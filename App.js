53fa9a1616c0093c7c6de41abb55572059a5c4fd
Isaac Householder <isaachouseholder@gmail.com>
2020-12-21 17:05:40 -0800

    Welcome Screen done

--- App.js
+++ App.js
@@ -1,35 +1,42 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import ViewImageScreen from "./screens/ViewImageScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center", // main axis
-        alignItems: "center", // secondary axis
-      }}
-    >
+    <>
+      <WelcomeScreen />
+      {/* <ViewImageScreen />
       <View
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
+          backgroundColor: "white",
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "center", // main axis
+          alignItems: "center", // secondary axis
         }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          position: "absolute",
-          top: 20,
-          left: 20,
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-    </View>
+      >
+        <View
+          style={{
+            backgroundColor: "dodgerblue",
+            width: 100,
+            height: 100,
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: "gold",
+            position: "absolute",
+            top: 20,
+            left: 20,
+            width: 100,
+            height: 100,
+          }}
+        />
+        <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      </View>
+       */}
+    </>
   );
 }
---
+++ screens/ViewImageScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function App() {
+  return (
+    <Text>View Image Screen</Text>
+  );
+}
