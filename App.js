fb04b136cceeefaca91f4fe345ac8e5368061d10
Isaac Householder <isaachouseholder@gmail.com>
2020-12-28 22:55:43 -0800

    AppTextInput created and finished

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { ImageBackground, View } from "react-native";
+import React, { useState } from "react";
+import { ImageBackground, View, Text, TextInput } from "react-native";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import MyAccountScreen from "./app/screens/MyAccountScreen";
-import Screen from "./app/components/Screen";
 import ListItem from "./app/components/ListItem";
+import AppTextInput from "./app/components/AppTextInput";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput icon="email" placeholder="username"/>
+    </Screen>
+  );
 }
--- app/congif/colors.js
+++ app/congif/colors.js
@@ -3,6 +3,7 @@ export default {
   secondary: "#4ECDC4",
   black: "#000",
   white: "#fff",
+  dark: "#0c0c0c",
   medium: "#6E6969",
   light: "#f8f4f4",
   danger: "#ff5252"
