c9b278867fcee5edb7b3cd3398c6d3c54be51f62
Isaac Householder <isaachouseholder@gmail.com>
2020-12-29 14:16:47 -0800

    login screen created

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet } from "react-native";
+import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
 
 function LoginScreen(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
-    <Screen>
+    <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-      <AppTextInput 
-      autoCapitalize="none"
-      autoCorrect={false}
-      icon="email"
-      keyboardType="email-address"
-      placeholder="Email"
-      textContentType="emailAddress"
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="email"
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+        textContentType="emailAddress"
+        placeholder="Email"
       />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="lock"
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Password"
+        secureTextEntry
+        textContentType="password"
+      />
+      <AppButton title="login" onPress={() => console.log(email, password)} />
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
   logo: {
     width: 80,
     height: 80,
