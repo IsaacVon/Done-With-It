c58797c9f93a0d1ae8daf011c5ea2b6bd2d0931e
Isaac Householder <isaachouseholder@gmail.com>
2020-12-28 23:01:33 -0800

    refactored and simplified styling

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -3,32 +3,27 @@ import { View, StyleSheet } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import colors from "../congif/colors";
+import defaultStyles from "../congif/styles"
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
     <View style={styles.container}>
       {icon && (
-        <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon}/>
+        <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon}/>
       )}
-      <TextInput style={styles.TextInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
     width: "100%",
     padding: 15,
     marginVertical: 10,
-  },
-  textInput: {
-    color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
   },
   icon: {
     marginRight: 10,
