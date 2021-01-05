73f3d1acc89332f227167508b00d7f687061078b
Isaac Householder <isaachouseholder@gmail.com>
2021-01-04 17:24:45 -0800

    cache layer added

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-
-import navigationTheme from "./app/navigation/navigationTheme";
-import AppNavigator from "./app/navigation/AppNavigator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log("person: ", person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  demo();
+  return null;
 }
